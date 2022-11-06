022c0d40ed40a75f6afb91a1ef8f95f4569b0b86
Artem Averin <artem-averin-91@mail.ru>
2022-11-06 18:03:34 +0300

    1. Динамический список на массиве. [#158]

--- src/main/java/ru/job4j/collection/SimpleArrayList.java
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -21,8 +21,7 @@ public class SimpleArrayList<T> implements SimpleList<T> {
 
     @Override
     public T set(int index, T newValue) {
-        Objects.checkIndex(index, size);
-        T res = container[index];
+        T res = get(index);
         container[index] = newValue;
         return res;
     }
