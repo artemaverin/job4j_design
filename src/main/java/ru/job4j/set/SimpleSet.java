05386dd404b8a0f3a4832b1b3d6bf58e033616a1
Artem Averin <artem-averin-91@mail.ru>
2022-11-13 18:54:18 +0300

    1. Реализовать коллекцию Set на массиве [#996]

---
+++ src/main/java/ru/job4j/set/SimpleSet.java
@@ -0,0 +1,58 @@
+package ru.job4j.set;
+
+import ru.job4j.collection.SimpleArrayList;
+import ru.job4j.collection.SimpleList;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleSet<T> implements Set<T> {
+
+    private SimpleArrayList<T> set = new SimpleArrayList<>(0);
+
+    @Override
+    public boolean add(T value) {
+        boolean res = true;
+        for (T t: set) {
+            if (Objects.equals(value, t)) {
+                res = false;
+                break;
+            }
+        }
+        set.add(value);
+        return res;
+    }
+
+    @Override
+    public boolean contains(T value) {
+        boolean res = false;
+        for (T t: set) {
+            if (Objects.equals(value, t)) {
+                res = true;
+                break;
+            }
+        }
+        return res;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+
+        return new Iterator<T>() {
+            private int i = 0;
+            @Override
+            public boolean hasNext() {
+                return i < set.size();
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return set.get(i++);
+            }
+        };
+    }
+}
