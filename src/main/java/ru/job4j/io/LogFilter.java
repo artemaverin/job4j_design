d1c4f4f76f2e41926e8f704d509648ab88dd0e2e
Artem Averin <artem-averin-91@mail.ru>
2022-11-21 22:53:23 +0300

    0.3. BufferedReader. [#252489 #362847]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -2,6 +2,7 @@ package ru.job4j.io;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,13 +10,14 @@ import java.util.stream.Collectors;
 public class LogFilter {
     public List<String> filter(String file) {
         List<String> res = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-            res = in.lines()
-                    .filter(e -> e.substring(e.length() - 8, e.length() - 5).equals("404"))
-                    .collect(Collectors.toList());
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (file != null) {
+            try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+                res = in.lines()
+                        .filter(e -> e.contains("\" 404 "))
+                        .collect(Collectors.toList());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         return res;
     }
@@ -23,6 +25,6 @@ public class LogFilter {
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter();
         List<String> log = logFilter.filter("log.txt");
-        System.out.println(log);
+        log.forEach(System.out::println);
     }
 }
