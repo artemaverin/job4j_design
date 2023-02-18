53b5c3e5506f3976a2d20b0f6146b60b02ecc721
Artem Averin <artem-averin-91@mail.ru>
2023-02-18 19:42:36 +0300

    1. Inner join [#373617 #392881]

--- db/inner join.sql
+++ db/inner join.sql
@@ -1,42 +1,38 @@
-create table position(
+create table job_position(
 	position_id serial primary key,
 	title text not NULL
 );
 
 create table employee (
-    employee_id SERIAL primary key,
+    employee_id serial primary key,
 	first_name text,
 	last_name text,
 	department varchar(255),
-	position_id int references position(position_id)
+	position_id int references job_position(position_id)
 );
 
-insert into position (title)
-values
-('manager'),
-('hed of dep.'),
-('specialist'),
-('worker');
+insert into job_position (title) values ('manager');
+insert into job_position (title) values ('hed of dep.');
+insert into job_position (title) values ('specialist');
+insert into job_position (title) values ('worker');
 
-insert into employee (first_name, last_name, department, position_id)
-values
-('Jack', 'Blak', 'customer service', 3),
-('Tom', 'Hardy', 'warranty', 1),
-('Sigurdsson', null, 'maintenance', 4),
-('Ilon', 'Musk', 'administration', 2),
-('Bruce', 'Li', 'legal service', 1),
-('Billy', 'Jean', 'customer service', 1),
-('Eva', 'Green', 'maintenance', 3);
+insert into employee (first_name, last_name, department, position_id) values ('Jack', 'Blak', 'customer service', 3);
+insert into employee (first_name, last_name, department, position_id) values ('Tom', 'Hardy', 'warranty', 1);
+insert into employee (first_name, last_name, department, position_id) values ('Sigurdsson', null, 'maintenance', 4);
+insert into employee (first_name, last_name, department, position_id) values ('Ilon', 'Musk', 'administration', 2);
+insert into employee (first_name, last_name, department, position_id) values ('Bruce', 'Li', 'legal service', 1);
+insert into employee (first_name, last_name, department, position_id) values ('Billy', 'Jean', 'customer service', 1);
+insert into employee (first_name, last_name, department, position_id) values ('Eva', 'Green', 'maintenance', 3);
 
-/**
-select e.first_name, e.last_name, e.department, p.title from 
-position p join employee e on p.position_id = e.position_id
-order by department
+
+select e.first_name, e.last_name, e.department, p.title
+from job_position p join employee e on p.position_id = e.position_id
+order by department;
 
 select e.first_name, e.last_name, e.department, p.title from 
-position p join employee e on p.position_id = e.position_id
-where e.last_name is null
+job_position p join employee e on p.position_id = e.position_id
+where e.last_name is null;
 
 select e.first_name, e.last_name, e.department, p.title from 
-position p join employee e on p.position_id = e.position_id
-where department like '%service'
+job_position p join employee e on p.position_id = e.position_id
+where department like '%service';
